3e4d928b1a6695a8468264d46394e2099c88d4f1
zizima100 <mr.zizima@gmail.com>
2021-11-08 16:56:44 -0300

    Posts Iniciais

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
 const addButton = document.getElementById("btn");
 const divContainer = document.querySelector("#div-container");
-const url = 'https://jsonplaceholder.typicode.com/posts/';
-let nClicks = 1;
+const getPostURL = id => `https://jsonplaceholder.typicode.com/posts/${id}`;
+const posts = [];
+const POSTS_INICIAIS = 3;
+let nextId = POSTS_INICIAIS + 1;
 
-const fetchPostJSON = async () => {
-  const response = await fetch(url + nClicks);
-  const data = await response.json();
-  return data;
+const getNextPost = () => {
+  fetch(getPostURL(nextId))
+    .then(response => response.json())
+    .then(data => makePost(data));
+  nextId++;
 }
 
-const addDiv = () => {
+const makePost = post => {
   const newDiv = document.createElement("div");
   newDiv.className = 'new-div';
+
   const newTitle = document.createElement("h1");
   newTitle.className = 'new-title';
+  newTitle.innerHTML = post.title;
+  
   const newParagraph = document.createElement("p");
   newParagraph.className = 'new-paragraph';
-
-  fetchPostJSON().then(data => {
-    newTitle.innerHTML = data.title;
-    newParagraph.innerHTML= data.body;
-    nClicks++;
-    newDiv.appendChild(newTitle);
-    newDiv.appendChild(newParagraph);
-    divContainer.appendChild(newDiv);
-  });
+  newParagraph.innerHTML= post.body;
+  
+  const newFooter = document.createElement("span");
+  newFooter.className = 'new-footer';
+  newFooter.innerHTML = post.id;
+  
+  newDiv.appendChild(newTitle);
+  newDiv.appendChild(newParagraph);
+  newDiv.appendChild(newFooter);
+  
+  divContainer.appendChild(newDiv);
 }
 
-addButton.addEventListener("click", addDiv);
+for(i = 1; i <= POSTS_INICIAIS; i++) {
+  posts.push(fetch(getPostURL(i)).then(data => data.json()))
+}
+Promise.all(posts)
+.then(posts => {
+  posts.forEach(post => {
+    makePost(post);
+  });
+})
+
+addButton.addEventListener("click", getNextPost);
