09797102d30064846821df59aac1d45bcfac507c
zizima100 <mr.zizima@gmail.com>
2021-11-08 12:27:59 -0300

    Upgrade Fetch

--- script.js
+++ script.js
@@ -1,40 +1,23 @@
 const addButton = document.getElementById("btn");
 const divContainer = document.querySelector("#div-container");
-
+const url = 'https://jsonplaceholder.typicode.com/posts/';
 let nClicks = 1;
 
-// -------- Fetch Funcionando normalmente abaixo. --------
-// const addDiv = () => {
-//   const newDiv = document.createElement("div");
-//   newDiv.className = 'new-div';
+const fetchPostJSON = async () => {
+  const response = await fetch(url + nClicks);
+  const data = await response.json();
+  return data;
+}
 
-//   fetch('https://jsonplaceholder.typicode.com/posts/' + nClicks)
-//     .then(response => response.json())
-//     .then(json => {
-//       newDiv.innerHTML = JSON.stringify(json);
-//       nClicks++;
-
-//       divContainer.appendChild(newDiv);
-//     });
-// }
-
-// -------- Tentativa de fazer o fetch acontecer dentro de uma função async. --------
-// NÃO FUNCIONAL!
 const addDiv = () => {
   const newDiv = document.createElement("div");
   newDiv.className = 'new-div';
 
-  const post = async function fetchPostJSON() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts/' + nClicks);
-    const post = await response.json();
-    console.log(JSON.stringify(post));
-    return post;
-  }
-
-  newDiv.innerHTML = JSON.stringify(post);
-  nClicks++;
-
-  divContainer.appendChild(newDiv);
+  fetchPostJSON().then(data => {
+    newDiv.innerHTML = JSON.stringify(data);
+    nClicks++;
+    divContainer.appendChild(newDiv);
+  });
 }
 
 addButton.addEventListener("click", addDiv);
