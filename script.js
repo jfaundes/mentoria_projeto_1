5d8e8052c6f897bbe7f16b3b331abd6f417b7a86
zizima100 <mr.zizima@gmail.com>
2021-11-07 23:55:51 -0300

    Primeiro Fetch Undefined

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+const addButton = document.getElementById("btn");
+const divContainer = document.querySelector("#div-container");
+
+var nClicks = 1;
+var post;
+
+const addDiv = () => {
+  const newDiv = document.createElement("div");
+
+  fetch('https://jsonplaceholder.typicode.com/posts/' + nClicks)
+  .then((response) => response.json())
+  .then((json) => post = json);
+
+  newDiv.className = 'newDiv';
+  newDiv.innerHTML = JSON.stringify(post);
+  nClicks++;
+
+  divContainer.appendChild(newDiv);
+}
+
+addButton.addEventListener("click", addDiv);
