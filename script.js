a8df8c8c81406279e437e307901be68ee8ddeda5
Julio Faundes <julio.minguini_dextra@prestador.globo>
2021-11-09 11:19:53 -0300

    Title Fix

--- script.js
+++ script.js
@@ -7,6 +7,29 @@ let nextId = 1;
 
 const getPostURL = id => `https://jsonplaceholder.typicode.com/posts/${id}`;
 
+const printPost = post => {
+  const newContainer = document.createElement("article");
+  newContainer.className = 'post-container';
+
+  const newH1 = document.createElement("h1");
+  newH1.className = 'post-title';
+  newH1.innerHTML = post.title;
+  
+  const newParagraph = document.createElement("p");
+  newParagraph.className = 'post-content';
+  newParagraph.innerHTML= post.body;
+  
+  const newFooter = document.createElement("footer");
+  newFooter.className = 'post-footer';
+  newFooter.innerHTML = post.id;
+  
+  newContainer.appendChild(newH1);
+  newContainer.appendChild(newParagraph);
+  newContainer.appendChild(newFooter);
+  
+  divContainer.appendChild(newContainer);
+}
+
 const fetchPosts = n => {
   for(i = 0; i < n; i++) {
     posts.push(fetch(getPostURL(nextId))
@@ -33,33 +56,6 @@ const getFivePosts = () => {
   fetchPosts(5);
 }
 
-const printPost = post => {
-  const newContainer = document.createElement("article");
-  newContainer.className = 'post-container';
-
-  const newTitle = document.createElement("title");
-  const newH1 = document.createElement("h1");
-  newH1.className = 'post-title';
-  newH1.innerHTML = post.title;
-  
-  const newParagraph = document.createElement("p");
-  newParagraph.className = 'post-content';
-  newParagraph.innerHTML= post.body;
-  
-  const newFooter = document.createElement("footer");
-  newFooter.className = 'post-footer';
-  newFooter.innerHTML = post.id;
-  
-  newH1.appendChild(newTitle);
-  newContainer.appendChild(newH1);
-  newContainer.appendChild(newParagraph);
-  newContainer.appendChild(newFooter);
-  
-  divContainer.appendChild(newContainer);
-}
-
 getThreePosts();
-
 loadOneBtn.addEventListener("click", getOnePost);
 loadFiveBtn.addEventListener("click", getFivePosts);
-
