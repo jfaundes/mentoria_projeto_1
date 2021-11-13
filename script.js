8b164015f724a2d9cec947751d3969a30c8e6d0f
zizima100 <mr.zizima@gmail.com>
2021-11-12 22:38:41 -0300

    Await implementado + catch

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
 const loadOneBtn = document.getElementById("load-one-btn");
 const loadFiveBtn = document.getElementById("load-five-btn");
 const divContainer = document.querySelector("#div-container");
-const posts = [];
-const POSTS_INICIAIS = 3;
-let nextId = 1;
+const POSTS_URL = `https://jsonplaceholder.typicode.com/posts/`;
+let currentId = 0;
 
-const getPostURL = id => `https://jsonplaceholder.typicode.com/posts/${id}`;
+const getAllPosts = async () => {
+  const response = await fetch(POSTS_URL);
+  const posts = await response.json();
+  return posts;
+};
+const postsPromise = getAllPosts();
 
-const printPost = post => {
+const printPosts = async nPosts => {
+  try {
+    const postsArray = await postsPromise;
+    for(i = 0; i < nPosts; i++) {
+      makePostContainer(postsArray[currentId]);
+      currentId++;
+    }
+  } catch (e) {
+    alert(`ERRO!`)
+    console.log(e);
+  }
+};
+
+const makePostContainer = post => {
   const newContainer = document.createElement("article");
   newContainer.className = 'post-container';
 
   const newH1 = document.createElement("h1");
   newH1.className = 'post-title';
   newH1.innerHTML = post.title;
-  
+
   const newParagraph = document.createElement("p");
   newParagraph.className = 'post-content';
-  newParagraph.innerHTML= post.body;
-  
+  newParagraph.innerHTML = post.body;
+
   const newFooter = document.createElement("footer");
   newFooter.className = 'post-footer';
   newFooter.innerHTML = post.id;
-  
+
   newContainer.appendChild(newH1);
   newContainer.appendChild(newParagraph);
   newContainer.appendChild(newFooter);
-  
+
   divContainer.appendChild(newContainer);
 }
 
-const getPosts = n => {
-  for(i = 0; i < n; i++) {
-    posts.push(fetch(getPostURL(nextId))
-      .then(data => data.json())
-      .then(nextId++));
-  }
-  Promise.all(posts.slice(-n))
-  .then(posts => {
-    posts.forEach(post => {
-      printPost(post);
-    });
-  })
-}
-
-getPosts(3);
-loadOneBtn.addEventListener("click", () => getPosts(1));
-loadFiveBtn.addEventListener("click", () => getPosts(5));
+printPosts(3);
+loadOneBtn.addEventListener("click", () => printPosts(1));
+loadFiveBtn.addEventListener("click", () => printPosts(5));
