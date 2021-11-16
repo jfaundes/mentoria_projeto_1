d95ae595bdd971ce12e136ae539b6c1bf6361db8
zizima100 <mr.zizima@gmail.com>
2021-11-16 11:13:02 -0300

    Módulo PostFactory

---
+++ postFactory.js
@@ -0,0 +1,36 @@
+function postFactory(post) {
+    function makeContainer() {
+        const container = document.createElement('article');
+        container.className = 'post-container';
+        return container;
+    }
+
+    function makeTitle(titleContent) {
+        const title = document.createElement('h1');
+        title.className = 'post-title';
+        title.innerHTML = titleContent;
+        return title;
+    }
+
+    function makeParagraph(paragraphContent) {
+        const paragraph = document.createElement('p');
+        paragraph.className = 'post-content';
+        paragraph.innerHTML = paragraphContent;
+        return paragraph;
+    }
+
+    function makeFooter(footerContent) {
+        const footer = document.createElement('footer');
+        footer.className = 'post-footer';
+        footer.innerHTML = footerContent;
+        return footer;
+    }
+
+    const container = makeContainer();
+    container.appendChild(makeTitle(post.title));
+    container.appendChild(makeParagraph(post.body));
+    container.appendChild(makeFooter(post.id));
+    return container;
+}
+
+export default postFactory;
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+import postFactory from "./postFactory.js";
+
+const divContainer = document.querySelector("#div-container");
 const loadOneBtn = document.getElementById("load-one-btn");
 const loadFiveBtn = document.getElementById("load-five-btn");
-const divContainer = document.querySelector("#div-container");
 const POSTS_URL = `https://jsonplaceholder.typicode.com/posts/`;
 let currentId = 0;
 
@@ -14,38 +16,15 @@ const postsPromise = getAllPosts();
 const printPosts = async nPosts => {
   try {
     const postsArray = await postsPromise;
-    for(i = 0; i < nPosts; i++) {
-      makePostContainer(postsArray[currentId]);
+    for(let i = 0; i < nPosts; i++) {
+      divContainer.appendChild(postFactory(postsArray[currentId]));
       currentId++;
     }
   } catch (e) {
-    alert(`ERRO!`)
+    alert(e);
     console.log(e);
   }
 };
-
-const makePostContainer = post => {
-  const newContainer = document.createElement("article");
-  newContainer.className = 'post-container';
-
-  const newH1 = document.createElement("h1");
-  newH1.className = 'post-title';
-  newH1.innerHTML = post.title;
-
-  const newParagraph = document.createElement("p");
-  newParagraph.className = 'post-content';
-  newParagraph.innerHTML = post.body;
-
-  const newFooter = document.createElement("footer");
-  newFooter.className = 'post-footer';
-  newFooter.innerHTML = post.id;
-
-  newContainer.appendChild(newH1);
-  newContainer.appendChild(newParagraph);
-  newContainer.appendChild(newFooter);
-
-  divContainer.appendChild(newContainer);
-}
 
 printPosts(3);
 loadOneBtn.addEventListener("click", () => printPosts(1));
