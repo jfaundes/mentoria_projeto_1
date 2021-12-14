b795f7857481130ce959b1a8bc77c008eb04acd0
Julio Faundes <julio.minguini_dextra@prestador.globo>
2021-12-14 11:49:48 -0300

    feat: postNewPost Async Await Refatoração

--- events/postNewPost.js
+++ events/postNewPost.js
@@ -2,20 +2,20 @@ import postSetup from "../modules/post.js";
 import { showNewPostCard } from "../script.js";
 
 const postNewPost = async post => {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify(post),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    }).then(async (response) => {
-        postSetup(await response.json());
+    try {
+        const newPost = await fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify(post),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        postSetup(await newPost.json());
         showNewPostCard();
-    })
-    .catch((error) => {
-        console.log(error)
+    } catch (error) {
+        console.log(error);
         alert('deu erro');
-    });
+    }
 }
 
 export { postNewPost };
