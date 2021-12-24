7e76063699c119425ba96ee009a8f08bef34747c
Julio Faundes <mr.zizima@gmail.com>
2021-12-24 12:28:32 -0300

    fix: Removed Commented Code

--- events/deletePost.js
+++ events/deletePost.js
@@ -1,11 +1,8 @@
-// import { postsPromise } from "../script.js";
-
 const deletePost = async post => {
   try {
     await fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}`, {
       method: 'DELETE',
     });
-    // await removePostFromArray(post.id);
     removePostFromHTML(post.id)
   } catch (error) {
     console.log(`Deu erro! ${error}`);
@@ -16,13 +13,6 @@ const removePostFromHTML = id => {
   const postCard = document.getElementById(`card${id}`);
   postCard.remove();
 }
-
-// Estou com dificuldades em importar uma promisse e atualizar o array.
-
-// const removePostFromArray = id => {
-//     const postsArray = await postsPromise;
-//     console.log(postsArray);
-// }
 
 export {
   deletePost
