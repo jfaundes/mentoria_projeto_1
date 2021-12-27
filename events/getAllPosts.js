6f4374b637409e164f77a2e910421470d9d27115
Julio Faundes <mr.zizima@gmail.com>
2021-12-27 11:30:03 -0300

    fix: removed forgotten line

--- events/getAllPosts.js
+++ events/getAllPosts.js
@@ -1,7 +1,6 @@
 const getAllPosts = async () => {
   window.postsArray = await fetch('https://jsonplaceholder.typicode.com/posts/')
     .then(response => response.json());
-  window.teste = `teste`;
 };
 
 export {
