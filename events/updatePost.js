ed2aa692f01c41b4216563b5b286d385ca3798f3
Julio Faundes <mr.zizima@gmail.com>
2021-12-27 11:28:48 -0300

    feat: updatedPost Adaptação var Global

--- events/updatePost.js
+++ events/updatePost.js
@@ -4,30 +4,32 @@ import {
 
 const updatePost = async post => {
   try {
-    const updatedPostResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}`, {
-      method: 'PUT',
-      body: JSON.stringify(post),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
+    const updatedPostResponse =
+      await fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(post),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+
     const updatedPost = await updatedPostResponse.json()
     updateHTMLPostTitle(updatedPost);
     updateHTMLPostContent(updatedPost);
     stopEdt(updatedPost.id);
   } catch (error) {
-    console.log(`Erro ao fazer o update do post!\n${error}`);
+    console.error(`Erro ao fazer o update do post!\n${error}`);
   }
 }
 
-const updateHTMLPostTitle = post => {
-  const postTitle = document.getElementById(`card__post-title${post.id}`);
-  postTitle.innerHTML = post.title;
+const updateHTMLPostTitle = updatedPost => {
+  const postTitle = document.getElementById(`card__post-title${updatedPost.id}`);
+  postTitle.innerHTML = updatedPost.title;
 }
 
-const updateHTMLPostContent = post => {
-  const postContent = document.getElementById(`card__post-content${post.id}`);
-  postContent.innerHTML = post.body;
+const updateHTMLPostContent = updatedPost => {
+  const postContent = document.getElementById(`card__post-content${updatedPost.id}`);
+  postContent.innerHTML = updatedPost.body;
 }
 
 export {
