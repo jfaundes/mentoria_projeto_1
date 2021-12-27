5a134c5b81f7380b96e6dcc116b9d64143f3a2f5
Julio Faundes <mr.zizima@gmail.com>
2021-12-27 11:27:48 -0300

    feat: DeletePost Adapatado var Global

--- events/deletePost.js
+++ events/deletePost.js
@@ -3,15 +3,20 @@ const deletePost = async post => {
     await fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}`, {
       method: 'DELETE',
     });
-    removePostFromHTML(post.id)
+    removePostFromHTML(post.id);
+    removePostFromArray(post.id);
   } catch (error) {
-    console.log(`Deu erro! ${error}`);
+    console.error(`Deu erro! ${error}`);
   }
 }
 
 const removePostFromHTML = id => {
   const postCard = document.getElementById(`card${id}`);
   postCard.remove();
+}
+
+const removePostFromArray = id => {
+  window.postsArray[id-1] = {};
 }
 
 export {
